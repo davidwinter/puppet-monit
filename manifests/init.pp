72d3f0c2bffccc08f22393fad4b7c50a22b6b0cc
David Winter <i@djw.me>
2013-03-28 10:55:43 +0000

    puppet-lint passing.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,26 @@
+# == Class: monit
+#
+# See README.md
+#
 class monit (
-		$template = 'monit/monitrc.erb',
-		$hdd = '/dev/sda',
-		$notification_email,
-		$system_hostname,
-	) {
+  $template           = 'monit/monitrc.erb',
+  $system_hostname    = 'localhost',
+  $hdd                = '/dev/sda',
+  $notification_email = '',
+) {
+  package { 'monit':
+    ensure => present,
+  }
 
-	package { 'monit':
-		ensure => present,
-	}
+  service { 'monit':
+    ensure  => running,
+    require => Package['monit'],
+  }
 
-	service { 'monit':
-		ensure  => running,
-		require => Package['monit'],
-	}
-
-	file { '/etc/monit/monitrc':
-		ensure  => file,
-		content => template($template),
-		require => Package['monit'],
-		notify  => Service['monit'],
-	}
+  file { '/etc/monit/monitrc':
+    ensure  => file,
+    content => template($template),
+    require => Package['monit'],
+    notify  => Service['monit'],
+  }
 }
